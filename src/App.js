8734d638d3b814fd999ddd7d0424850316fb7474
Christian Cheng <christiancheng15@gmail.com>
2023-09-01 23:33:21 +1000

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Home from './pages/Home/Home.js';
+import AboutUs from './pages/AboutUs/AboutUs.js';
+import Market from './pages/Market/Market.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home  />}/>
+        <Route path="/about" element={<AboutUs  />}/>
+        <Route path="/market" element={<Market  />}/>
+      </Routes>
+    </Router>
+    
   );
 }
 
