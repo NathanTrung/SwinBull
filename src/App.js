2141d7539b31f0b50f8e5f901e931d0036ee6c48
Christian Cheng <christiancheng15@gmail.com>
2023-09-02 23:26:03 +1000

    CHRISTIAN FIX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use Routes instead of Switch
 import NavBar from './navbar';
-import Home from './pages/Home/Home'; // Create a Home component
 import Market from './pages/Market/Market'; // Create a Market component
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <div>
         <NavBar />
         <Routes> {/* Use Routes instead of Switch */}
-          <Route path="/" element={<Home />} /> {/* Use element prop */}
           <Route path="/market" element={<Market />} /> {/* Use element prop */}
           {/* Add more routes for other pages */}
         </Routes>
